f6a457d14d9ddaab5f8e15665e4d995a6d41b5f1
sachinchauhan010 <010sssachin@gmail.com>
2024-03-11 00:32:26 +0530

    Project Section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,19 +1,86 @@
+import { projectList } from "../utils/Data";
+import { Link } from "react-router-dom";
 const Project = () => {
+  const projects = projectList;
   return (
-    <>
-      <article class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-40 max-w-sm mx-auto mt-24">
-        <img
-          src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a"
-          alt="University of Southern California"
-          class="absolute inset-0 h-full w-full object-cover"
-        />
-        <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-        <h3 class="z-10 mt-3 text-3xl font-bold text-white">Paris</h3>
-        <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">
-          City of love
-        </div>
-      </article>
-    </>
+    <div className="font-Roboto m-5">
+      <div className="flex justify-evenly items-center ">
+        <p className="text-xl md:text-2xl lg:text-4xl font-semibold md:font-bold text-center text-fuchsia-700">
+          Projects that I had made
+        </p>
+        <Link to={"/contact"}>
+          <button className="bg-gradient-to-r from-purple-500 rounded-md to-pink-500 px-4 py-2 my-3 md:my-8 text-lg font-semibold text-white hover:text-blue-900 text-center">
+            Let's Discuss Your Project
+          </button>
+        </Link>
+      </div>
+      <div className="flex flex-wrap justify-center">
+        {projects.map((item) => (
+          <div className="flex flex-col justify-start relative m-5 ">
+            <img
+              src={item.imageLink}
+              alt=""
+              className="h-[200px] w-[400px] md:h-[320px] md:w-[600px] rounded-lg brightness-75 hover:brightness-50"
+            />
+
+            <div className="absolute top-10 left-12 text-fuchsia-200 w-full transition duration-500 hover:scale-110">
+              <p className="text-xl md:text-2xl lg:text-4xl font-semibold">
+                {item.webName}
+              </p>
+              <p className="text-2xl md:text-4xl lg:text-8xl font-semibold absolute right-20 top-0">
+                {item.id}
+              </p>
+
+              <Link
+                target="_blank"
+                to={item.liveLink}
+                className="flex md:text-xl lg:text-2xl font-semibold  m-2 gap-2"
+              >
+                Live Link
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-link"
+                >
+                  <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
+                  <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
+                </svg>
+              </Link>
+              <Link
+                target="_blank"
+                to={item.gitLink}
+                className="flex md:text-xl lg:text-2xl font-semibold m-2 gap-2"
+              >
+                Github Link
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-link"
+                >
+                  <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
+                  <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
+                </svg>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="center"></div>
+    </div>
   );
 };
 export default Project;
--- src/utils/Data.js
+++ src/utils/Data.js
@@ -2,7 +2,39 @@ export const IMG_URL="https://github.com/sachinchauhan010/PortfolioProject/tree/
 
 export const projectList=[
     {
-        id:1,
-        liveLink:"https://netflixsachin.vercel.app/"
-    }
+        id:"01",
+        liveLink:"https://netflixsachin.vercel.app/",
+        gitLink:"https://github.com/sachinchauhan010/Netflix_clone",
+        imageLink:"https://raw.githubusercontent.com/sachinchauhan010/PortfolioProject/sachin/src/images/netflixClone.png",
+        webName:"Netflix Clone",
+    },
+    {
+        id:"02",
+        liveLink:"https://sachinpassword.vercel.app/",
+        gitLink:"https://github.com/sachinchauhan010/Password",
+        imageLink:"https://raw.githubusercontent.com/sachinchauhan010/PortfolioProject/sachin/src/images/passwordGenerator.png",
+        webName:"Password Generator",
+    },
+    {
+        id:"03",
+        liveLink:"https://hogarbook.vercel.app/",
+        gitLink:"https://github.com/sachinchauhan010/Hogar",
+        imageLink:"https://raw.githubusercontent.com/sachinchauhan010/PortfolioProject/sachin/src/images/HogarClone.png",
+        webName:"Hogar Clone",
+    },
+   
+    {
+        id:"04",
+        liveLink:"https://chauhansachin.vercel.app/",
+        gitLink:"https://github.com/sachinchauhan010/portfolio",
+        imageLink:"https://raw.githubusercontent.com/sachinchauhan010/PortfolioProject/sachin/src/images/portfolioWeb.png",
+        webName:"Portfolio",
+    },
+    {
+        id:"05",
+        liveLink:"https://sidcupfamilygolf.vercel.app/",
+        gitLink:"https://github.com/sachinchauhan010/sidcup_Family_Golf",
+        imageLink:"https://raw.githubusercontent.com/sachinchauhan010/PortfolioProject/sachin/src/images/sidCupClone.png",
+        webName:"Sid Cup Family Golf",
+    }, 
 ]
