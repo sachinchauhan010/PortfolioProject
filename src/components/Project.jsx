dfc5c05ef4728f9687bf63260f6f3b5ad003af8c
sachinchauhan010 <010sssachin@gmail.com>
2024-03-10 11:25:59 +0530

    Images added

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,32 +1,18 @@
 const Project = () => {
   return (
     <>
-      <div>
-        <div className="max-w-xs rounded-md shadow-md">
-          <img
-            src="https://source.unsplash.com/random/300x300/?2"
-            alt=""
-            className="object-cover object-center w-full rounded-t-md h-72"
-          />
-          <div className="flex flex-col justify-between p-6 space-y-8">
-            <div className="space-y-2">
-              <h2 className="text-3xl font-semibold tracki">
-                Donec lectus leo
-              </h2>
-              <p className="dark:text-gray-100">
-                Curabitur luctus erat nunc, sed ullamcorper erat vestibulum
-                eget.
-              </p>
-            </div>
-            <button
-              type="button"
-              className="flex items-center justify-center w-full p-3 font-semibold tracki rounded-md dark:bg-violet-400 dark:text-gray-900"
-            >
-              Read more
-            </button>
-          </div>
+      <article class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-40 max-w-sm mx-auto mt-24">
+        <img
+          src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a"
+          alt="University of Southern California"
+          class="absolute inset-0 h-full w-full object-cover"
+        />
+        <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+        <h3 class="z-10 mt-3 text-3xl font-bold text-white">Paris</h3>
+        <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">
+          City of love
         </div>
-      </div>
+      </article>
     </>
   );
 };
---
+++ src/utils/Data.js
@@ -0,0 +1,8 @@
+export const IMG_URL="https://github.com/sachinchauhan010/PortfolioProject/tree/sachin/src/images/";
+
+export const projectList=[
+    {
+        id:1,
+        liveLink:"https://netflixsachin.vercel.app/"
+    }
+]
