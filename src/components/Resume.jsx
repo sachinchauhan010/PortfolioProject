c49291696cec8edc9e70b7dab408cb7dfe1a6c1f
sachinchauhan010 <010sssachin@gmail.com>
2024-03-11 09:36:13 +0530

    Resume png Formate

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,8 +1,10 @@
+im
 const Resume=()=>{
     return(
        <>
        <div className="m-10">
         <p className="md:text-xl lg:text-2xl text-lg font-semibold text-blue-800 text-center">Make a look on my Resume</p>
+        <Link to={"/"}></Link>
        </div>
        </>
     )
