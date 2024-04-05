16dcae516a509ef961ba179c340f4be79362f385
sachinchauhan010 <010sssachin@gmail.com>
2024-04-06 02:14:58 +0530

    Mobile Screen Bug fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Outlet} from "react-router-dom";
 import Header from "./components/Header";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Project from "./components/Project";
+
 function App() {
   return (
     <div className="">
